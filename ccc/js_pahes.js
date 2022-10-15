4d41e6171d421b06216b68fa18e3fff8c07649ce
akisyy <85426407+akisyy@users.noreply.github.com>
2022-10-15 16:19:38 +0800

    Add files via upload

---
+++ ccc/js_pahes.js
@@ -0,0 +1,14 @@
+
+function backGroundChange1() {
+        const a = document.getElementById("big");
+        a.src = "IMG_2562.PNG";
+    }
+function backGroundChange2() {
+    const a = document.getElementById("big");
+    a.src = "left.png";
+
+}
+function backGroundChange3() {
+    const a = document.getElementById("big");
+    a.src = "IMG_2616.GIF";
+}
